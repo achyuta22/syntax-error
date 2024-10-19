c489098c2143ff9be339ad344909cb649afecdf1
RAVIHAL BHARAT ACHYUTA <136910474+achyuta22@users.noreply.github.com>
2024-10-20 00:13:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const io = require("socket.io")(server, {
         `https://eazeplace.com`,
         `https://eazeplace.com/`,
         `https://syntax-erro-frontend-g0046khtj-eazeplace.vercel.app` // Add your Vercel URL here
-       https://syntax-erro-frontend-git-main-eazeplace.vercel.app/
+       `https://syntax-erro-frontend-git-main-eazeplace.vercel.app`
 
       ],
     },
