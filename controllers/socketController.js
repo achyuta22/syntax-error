48a0addf2ea6f329cacdb75cf77fe3852e65be99
yomanthunder <vemareddy_sr@ee.iitr.ac.in>
2024-10-19 10:57:23 +0530

    feat: add sharedvariable

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,12 +1,28 @@
 // controllers/socketController.js
+let sharedVariable = 'initialValue';
+
 module.exports = (io) => {
     return (socket) => {
         console.log('A user connected:', socket.id);
+        let room; // Variable to store the room the user joined
 
         // Handle the 'joinRoom' event
-        socket.on('joinRoom', (room) => {
+        socket.on('joinRoom', (roomName) => {
+            room = roomName; // Store the room name in the room variable
             socket.join(room); // Join the specified room
             console.log(`User ${socket.id} joined room: ${room}`);
+        });
+
+        // Listen for changes to the variable from clients
+        socket.on('changeVariable', (newValue) => {
+            sharedVariable = newValue;
+            console.log('Shared variable has been updated:', sharedVariable);
+
+            // Broadcast the change to all clients in the room
+            if (room) {
+                console.log(`Broadcasting update to room: ${room}`);
+                io.to(room).emit('variableUpdated', sharedVariable);
+            }
         });
 
         // Handle the disconnect event
