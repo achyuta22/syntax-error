a4953c06dca14d504199cf4ae6a2c02793665ac6
yomanthunder <vemareddy_sr@ee.iitr.ac.in>
2024-10-22 05:18:52 +0530

    socketComponent cleanup , function initialisation

--- controllers/roomEvents.js
+++ controllers/roomEvents.js
@@ -5,25 +5,86 @@ and managaing all socket connections in socketController
 
 let rooms = {};
 //playStatusChanged,updateSongUrl,syncTimestamp
-function createRoom(){
 
-}
 // get room id which can used to join the room may be or we will just use room name 
-const createRoom = ()=>{
+const createRoom = ({ roomName, songUrl, playStatus,hostId})=>{
+    if(!rooms[roomName]){
+        rooms[roomName] = {
+            hostId : hostId,
+            peers : {},
+            songUrl:songUrl,
+            playStatus:playStatus
+        };
+        return true;
+    }
+    return false;
+}
+// Host changes song 
+//Update the current song, reset startTime, and notify peers to switch to the new song
+const changeSong = ()=>{
 
 }
-const joinRoom = ()=>{
+// takes roomName as input and give complete room with all the room data
+const getRoom = ({roomName})=>{
+    return rooms[roomName];
+}
+// joinRoom 
+//Send the current song, timestamp, and duration to new peers joining the room for synchronization.
+const joinRoom = ({roomName})=>{
+    let room = getRoom(roomName);
+}
+
+const addPeerToRoom = (roomName, peerId, currentTime) => {
+    if (rooms[roomName]) {
+        rooms[roomName].peers[peerId] = { peerId, currentTime };
+    }
+};
+
+const removePeerFromRoom = (roomName, peerId) => {
+    if (rooms[roomName]) {
+        delete rooms[roomName].peers[peerId];
+    }
+};
+
+const deleteRoom = (roomName) => {
+    if (rooms[roomName]) {
+        delete rooms[roomName];
+    }
+};
+//startSong
+//Store song ID, duration, and host's startTime and initialTimestamp.
+
+const startSong = ()=>{
+
+}
+//Dynamic Host Timestamp Calculation
+//Dynamically calculate the host’s current timestamp using startTime and initialTimestamp.
+const getHostTimestamp = ()=>{
+
+}
+
+// peer sync with host 
+//Sync peers' playback by adjusting their timestamp based on the host’s broadcasted timestamp.
+const syncWithHost = ({})=>{
+
+}
+
+// Function to invite people to the room
+const generateInviteLink = ()=>{
     
 }
-const playStatusChanged = ()=>{
-    
-}
-const updateSongUrl = ()=>{
-    
-}
-const syncTimestamp = ()=>{
-    
-}
-const getRoom = ()=>{
-    
+// shareInviteLink,
+//joinViaInvite - can use getRoom function and use the joinRoom functionality 
+//Writing a socket connection to notifyHost
+
+module.exports = {
+    createRoom,
+    changeSong,
+    getRoom,
+    joinRoom,
+    addPeerToRoom,
+    removePeerFromRoom,
+    deleteRoom,
+    startSong,
+    generateInviteLink,
 }
